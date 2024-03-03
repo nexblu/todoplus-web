de13764424ac5ddc1915231b519b8d2f61974744
nexblu <farras.pramudita@gmail.com>
2024-03-03 22:18:20 +0700

    feat(pages): add page 404 not found, login, register

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { BrowserRouter as Routers, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import NotFound from './pages/NotFound';
 
 const App = () => {
   return (
@@ -7,6 +10,9 @@ const App = () => {
       <Routers>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route path='*' element={<NotFound />} />
         </Routes>
       </Routers>
     </>
