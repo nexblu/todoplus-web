2bce11e356ece073e55d0fa9f642fcaef5acc927
nexblu <farras.pramudita@gmail.com>
2024-03-04 21:22:41 +0700

    fix(register): add validation on user register

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -1,25 +1,90 @@
-import { Button, Form } from 'react-bootstrap'
+import { useState } from 'react';
+import { Button, Form } from 'react-bootstrap';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const FormRegister = () => {
+    const [email, setEmail] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const [emailError, setEmailError] = useState(false);
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+
+    const successRegis = (text) => {
+        toast.success(text, {
+            position: "bottom-right"
+        });
+    };
+
+    const errorRegis = (text) => {
+        toast.error(text, {
+            position: "bottom-right"
+        });
+    };
+
+    const validationForm = () => {
+        let valid = true;
+
+        if (email === '') {
+            setEmailError(true);
+            valid = false;
+        } else {
+            setEmailError(false);
+        }
+
+        if (username === '') {
+            setUsernameError(true);
+            valid = false;
+        } else {
+            setUsernameError(false);
+        }
+
+        if (password === '') {
+            setPasswordError(true);
+            valid = false;
+        } else {
+            setPasswordError(false);
+        }
+
+        return valid;
+    }
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        const isValid = validationForm();
+
+        if (isValid) {
+            successRegis('Success Register');
+        } else {
+            errorRegis('Failed Register');
+        }
+    }
+
     return (
         <>
-            <Form className='m-3 text-light'>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form className='m-3 text-light' onSubmit={handleRegister}>
+                <Form.Group className="mb-3">
                     <Form.Label>Email Address</Form.Label>
-                    <Form.Control type="email" placeholder="Email Address" />
+                    <Form.Control type="text" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    {emailError && <Form.Text className="text-danger">Email is required</Form.Text>}
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
-                    <Form.Control type="text" placeholder="Username" />
+                    <Form.Control type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+                    {usernameError && <Form.Text className="text-danger">Username is required</Form.Text>}
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                    {passwordError && <Form.Text className="text-danger">Password is required</Form.Text>}
                 </Form.Group>
                 <a href="/" className='text-light'>Forgot Password</a>
-                <Button variant="primary" type="submit" className='mt-3'>
+                <Button variant="primary" className='mt-3' type='submit'>
                     Register
                 </Button>
+                <ToastContainer />
             </Form>
         </>
     );
