e893fb7bc10c678446a7f5fbd09bd2fc8ac17d67
nexblu <farras.pramudita@gmail.com>
2024-03-05 21:48:19 +0700

    fix(register): add email validator

--- src/components/FormRegister.jsx
+++ src/components/FormRegister.jsx
@@ -12,6 +12,27 @@ const FormRegister = () => {
     const [usernameError, setUsernameError] = useState(false);
     const [passwordError, setPasswordError] = useState(false);
 
+    const [messageEmailError, setMessageEmailError] = useState('');
+    const [messageUsernameError, setMessageUsernameError] = useState('');
+    const [messagePasswordError, setMessagePasswordError] = useState('');
+
+    const validateEmail = (email) => {
+        return fetch(`http://localhost:5000/todoplus/v1/email/${email}`)
+            .then(response => response.json())
+            .then(data => {
+                if (data['status_code'] === 200) {
+                    return true;
+                } else {
+                    console.log(data['status_code']);
+                    return false;
+                }
+            })
+            .catch(error => {
+                console.error('Terjadi kesalahan:', error);
+                return false;
+            });
+    }
+
     const successRegis = (text) => {
         toast.success(text, {
             position: "bottom-right",
@@ -32,13 +53,22 @@ const FormRegister = () => {
         if (email === '') {
             setEmailError(true);
             valid = false;
+            setMessageEmailError('Email Is Required.')
         } else {
-            setEmailError(false);
+            const validate = validateEmail(email);
+            if (validate) {
+                setEmailError(false)
+            } else {
+                setEmailError(true)
+                valid = false
+                setMessageEmailError('Email Is Not Valid.')
+            }
         }
 
         if (username === '') {
             setUsernameError(true);
             valid = false;
+            setMessageUsernameError('Username Is Required.')
         } else {
             setUsernameError(false);
         }
@@ -46,6 +76,7 @@ const FormRegister = () => {
         if (password === '') {
             setPasswordError(true);
             valid = false;
+            setMessagePasswordError('Password Is Required.')
         } else {
             setPasswordError(false);
         }
@@ -70,17 +101,17 @@ const FormRegister = () => {
                 <Form.Group className="mb-3">
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control type="text" placeholder="Email Address" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    {emailError && <Form.Text className="text-danger">Email Is Required.</Form.Text>}
+                    {emailError && <Form.Text className="text-danger">{messageEmailError}</Form.Text>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Username</Form.Label>
                     <Form.Control type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
-                    {usernameError && <Form.Text className="text-danger">Username Is Required.</Form.Text>}
+                    {usernameError && <Form.Text className="text-danger">{messageUsernameError}</Form.Text>}
                 </Form.Group>
                 <Form.Group className="mb-3">
                     <Form.Label>Password</Form.Label>
                     <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    {passwordError && <Form.Text className="text-danger">Password is required</Form.Text>}
+                    {passwordError && <Form.Text className="text-danger">{messagePasswordError}</Form.Text>}
                 </Form.Group>
                 <Button variant="primary" className='mt-3' type='submit'>
                     Register
